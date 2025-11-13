02d858107c1148b443338a8ccc1ab983d852946b
Ashwani Puri <puriashwa886@gmail.com>
2025-11-13 21:07:35 +0530

    fixed registration, login and logout. Application runs smoothly and doesnot crash on registration and login

--- server/auth.ts
+++ server/auth.ts
@@ -23,8 +23,8 @@ export const sessionMiddleware = session({
   cookie: {
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
     httpOnly: true,
-    secure: true, // Use secure cookies in production
-    sameSite: "none",
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   },
 });
 
